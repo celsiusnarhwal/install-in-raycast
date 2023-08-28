41b554a21af966b11e52e20d572195ae3fdb8fb4
celsius narhwal <hello@celsiusnarhwal.dev>
2023-08-28 13:22:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# "Install in Raycast" Button
+
+A button you can use to link to your [Raycast](https://raycast.com) extension's store page
+from its GitHub README.
+
+![Install in Raycast](https://installinraycast.celsiusnarhwal.dev/dark.svg) ![Install in Raycast](https://installinraycast.celsiusnarhwal.dev/light.svg)
+
+Make your own at [installinraycast.celsiusnarhwal.dev](https://installinraycast.celsiusnarhwal.dev).
+
+## License
+
+The contents of this repository are licensed under the [MIT License](LICENSE.md).
+
+This project is not affiliated with or endorsed by Raycast. "Raycast" and the Raycast logo are trademarks of Raycast Technologies Ltd.
---
+++ app/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function Home() {
+  enum Appearance {
+    System = "system",
+    Light = "light",
+    Dark = "dark",
+  }
+
+  function getButtonMarkup(url: string, appearance: Appearance) {
+    if (appearance === Appearance.System) {
+      return `<a href=${url}>
+          <picture>
+              <source media="(prefers-color-scheme: dark)" srcset="https://installinraycast.celsiusnarhwal.dev/dark.svg"/>
+              <source media="(prefers-color-scheme: light)" srcset="https://installinraycast.celsiusnarhwal.dev/light.svg"/>
+              <img alt="Install in Raycast" src="https://installinraycast.celsiusnarhwal.dev/light.svg"/>
+          </picture>
+          </a>`;
+    } else {
+      return `[![Install in Raycast](https://installinraycast.celsiusnarhwal.dev/${appearance}.svg)](${url})`;
+    }
+  }
+
+  const [url, setUrl] = React.useState("");
+  const [appearance, setAppearance] = React.useState(Appearance.System);
+
+  async function copyRaycastButton(url: string, appearance: Appearance) {
+    await window.navigator.clipboard.writeText(
+       getButtonMarkup(url, appearance)
+    );
+
+    const button = document.getElementById("copyButton");
+    button!.innerHTML = "Copied!";
+    setTimeout(() => {
+      button!.innerHTML = "Copy Markdown";
+    }, 1500);
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-black">
+      <div className="z-10 text-sm">
+        <Card className="min-w-[500px] max-w-5xl w-auto bg-white dark:bg-black">
+          <CardHeader>
+            <picture>
+              <source media="(prefers-color-scheme: dark)" srcSet="https://installinraycast.celsiusnarhwal.dev/dark.svg"/>
+              <source media="(prefers-color-scheme: light)" srcSet="https://installinraycast.celsiusnarhwal.dev/light.svg"/>
+              <img alt="Install in Raycast" src="https://installinraycast.celsiusnarhwal.dev/light.svg"/>
+          </picture>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="url">Raycast Store URL</Label>
+                  <Input
+                    className={"bg-white dark:bg-black"}
+                    id="url"
+                    placeholder="https://raycast.com/celsiusnarhwal/command-prompt"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="apperance">Appearance</Label>
+                  <Select
+                    value={appearance}
+                    onValueChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                      setAppearance(e.target.value as Appearance)
+                    }
+                  >
+                    <SelectTrigger
+                      className={"bg-white dark:bg-black"}
+                      id="apperance"
+                    >
+                      <SelectValue placeholder="System" />
+                    </SelectTrigger>
+                    <SelectContent
+                      position="popper"
+                      className={"bg-white dark:bg-black"}
+                    >
+                      <SelectItem
+                        value={Appearance.System}
+                        onClick={() => setAppearance(Appearance.System)}
+                      >
+                        System
+                      </SelectItem>
+                      <SelectItem
+                        value={Appearance.Light}
+                        onClick={() => setAppearance(Appearance.Light)}
+                      >
+                        Light
+                      </SelectItem>
+                      <SelectItem
+                        value="dark"
+                        onClick={() => setAppearance(Appearance.Dark)}
+                      >
+                        Dark
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button
+              id={"copyButton"}
+              onClick={() => copyRaycastButton(url, appearance)}
+            >
+              Copy Markup
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <footer
+        className={"fixed bottom-4 left-0 right-0 text-center text-gray-500"}
+      >
+        Copyright © {new Date().getFullYear()}{" "}
+        <a
+          href="https://celsiusnarhwal.dev"
+          className={
+            "hover:text-black dark:hover:text-white transition duration-200"
+          }
+        >
+          celsius narhwal
+        </a>
+        . Thank you kindly for your attention.
+        <br />
+        This project is not affiliated with or endorsed by Raycast.
+        <br />
+        "Raycast" and the Raycast logo are trademarks of Raycast Technologies
+        Ltd.
+      </footer>
+    </main>
+  );
+}
